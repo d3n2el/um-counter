5a1410182dffc6e7b3836a99d60eafeed830d6d1
d3n2el <118548099+d3n2el@users.noreply.github.com>
2024-08-23 22:05:20 +0200

    Add files via upload

---
+++ um.py
@@ -0,0 +1,23 @@
+import re
+import sys
+
+
+def main():
+    print(count(input("Text: ")))
+
+
+def count(s):
+    count=0
+    matches= re.findall(r"\bum\b", s)
+    print(matches)
+    for _ in matches:
+        count+=1
+    return count
+
+
+
+...
+
+
+if __name__ == "__main__":
+    main()
